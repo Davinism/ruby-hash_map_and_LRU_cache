96301228ab4bff8cb3f9e046092c6adf433871a2
Davin Kim <davinkim2015@u.northwestern.edu>
2016-07-22 16:05:58 -0700

    made crude working lru_cache

--- lib/p06_lru_cache.rb
+++ lib/p06_lru_cache.rb
@@ -15,6 +15,26 @@ class LRUCache
   end
 
   def get(key)
+    # my_link_pointer = returns_link_with_key(key)
+    if @map.include?(key)
+      link = @map[key]
+      @store.remove(key)
+      @store.insert(link.key, link.val)
+      link.val
+    else #adding new val
+      val = @prc.call(key)
+      new_link = Link.new(key, val)
+      @store.insert(key, val)
+      @map[key] = new_link
+
+      if count > @max #removing oldest element from linked list cache
+        removed_link_key = @store.first.key
+        @store.remove(removed_link_key)
+        @map.delete(removed_link_key)
+      end
+      val
+    end
+
   end
 
   def to_s
